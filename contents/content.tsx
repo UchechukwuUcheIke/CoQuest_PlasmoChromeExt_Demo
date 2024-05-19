4f0b7d4546cee7ed90b546c66d2df81c729df70e
Uche <uuchei2@illinois.edu>
2024-05-18 23:52:39 -0500

    Feat: Added ContentUI popup skeleton code

--- contents/content.tsx
+++ contents/content.tsx
@@ -1,42 +1,20 @@
-import createCache from "@emotion/cache"
-import { CacheProvider } from "@emotion/react"
-import Button from "@mui/material/Button"
-import Input from "@mui/material/Input"
-import Link from "@mui/material/Link"
-import Stack from "@mui/material/Stack"
-import Typography from "@mui/material/Typography"
-import { useState } from "react"
+import cssText from "data-text:~/contents/content.css"
 
-const styleElement = document.createElement("style")
 
-const styleCache = createCache({
-  key: "plasmo-mui-cache",
-  prepend: true,
-  container: styleElement
-})
+// Inject into the ShadowDOM
+export const getStyle = () => {
+  const style = document.createElement("style")
+  style.textContent = cssText
+  return style
+}
 
-export const getStyle = () => styleElement
 
-function PlasmoOverlay() {
-  const [data, setData] = useState("")
-
+const GoogleSidebar = () => {
   return (
-    <CacheProvider value={styleCache}>
-      <Stack minWidth={240} bgcolor={"white"} padding={2}>
-        <Typography variant="h6">
-          Welcome to your{" "}
-          <Link href="https://www.plasmo.com" target="_blank">
-            Plasmo
-          </Link>{" "}
-          Extension!
-        </Typography>
-        <Input onChange={(e) => setData(e.target.value)} value={data} />
-        <Button href="https://docs.plasmo.com" target="_blank">
-          View Docs
-        </Button>
-      </Stack>
-    </CacheProvider>
+      <button className="overlay">
+        Hi!
+      </button>
   )
 }
 
-export default PlasmoOverlay
+export default GoogleSidebar
