c1d3f4a84d7335fa4f9bfe6d76e50b3c9443aef1
Uche <uuchei2@illinois.edu>
2024-05-21 15:25:23 -0500

    Feat: Basic Functional Prototype of CoQuest Extension

--- contents/content.tsx
+++ contents/content.tsx
@@ -1,9 +1,14 @@
 import cssText from "data-text:~/contents/content.css"
-import { Paper, Stack, Box, Divider, Button, AppBar, Typography, IconButton, Icon, Toolbar } from "@mui/material"
+import { Paper, Stack, Box, Divider, Button, AppBar, Typography, IconButton, Accordion, Toolbar } from "@mui/material"
 import CloseIcon from '@mui/icons-material/Close';
 import SettingsIcon from '@mui/icons-material/Settings';
 import createCache from "@emotion/cache"
 import { CacheProvider } from "@emotion/react"
+
+import { useState, useEffect } from "react";
+
+
+
 
 
 const style = document.createElement("style")
@@ -17,41 +22,68 @@ const styleCache = createCache({
 
 // Inject into the ShadowDOM
 export const getStyle = () => {
+  console.log(style);
   return style
 }
 
 
-const GoogleSidebar = () => {
+const Workbench = () => {
+
+  const [workbenchOpen, setWorkbenchOpen] = useState(false);
+
+  function handleOnExit() {
+    setWorkbenchOpen(false);
+  }
+
+  function handleOnExport() {
+    window.open("https://www.wikipedia.org/", "_blank")
+  }
+
+  useEffect(() => {
+     //TODO: Change this to Plasmo's messaging tools for communication
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+      if (request.action === 'toggle-workbench-visibility') {
+        setWorkbenchOpen(true);
+        sendResponse({ status: 'success' });
+      }
+    });
+  },[])
+
   return (
+      (workbenchOpen &&
       <Box className = "overlay">
         <CacheProvider value={styleCache}>
-        <Paper className="paper">
+        <Paper className="paper" sx={{
+        display: 'flex'}}>
           <AppBar
-              sx={{position: "relative", justifyContent: 'center'}}>
+              sx={{position: "relative", justifyContent: 'center', height: "20%"}}>
             <Toolbar>
               <Typography margin="0px" sx={{mr: 2, flexGrow: 1}}>
                 CoQuest
               </Typography>
 
-              <IconButton sx={{backgroundColor: "grey" }}>
+              <IconButton onClick={handleOnExit}>
                 <CloseIcon/>
               </IconButton>
             </Toolbar>
           </AppBar>
-          <Box className="box">
-            
+          <Stack className="box" direction="row" spacing={2} justifyContent="space-around" alignItems="center">
+
+          <Button variant="contained">Research Question</Button>
+          <Button variant="contained">Persona</Button>
 
 
-          </Box>
-          <Divider orientation="vertical" variant="middle" />
+          </Stack>
+          <Divider orientation="vertical" variant="middle" flexItem/>
           <Stack className="stack" direction="column" justifyContent="center" alignItems="stretch" spacing={2}>
             <Button variant="contained"> Reset </Button>
-            <Button variant="contained"> Export </Button>
+            <Button variant="contained" onClick={handleOnExport}> Export </Button>
           </Stack>
         </Paper>
         </CacheProvider>
       </Box>
+      )
   )
 }
 
-export default GoogleSidebar
+export default Workbench
--- popup/index.tsx
+++ popup/index.tsx
@@ -1,5 +1,6 @@
 //import './index.css';
 import { useState, Fragment } from "react"
+import { sendToBackground, sendToContentScript } from "@plasmohq/messaging"
 import "./index.css"
 import { Button, Typography, Toolbar, IconButton, Stack, AppBar, Box } from '@mui/material';
 
@@ -14,7 +15,28 @@ const theme = createTheme({
 
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+
+  async function  handleExploreTopics() {
+    //TODO: Change this to Plasmo's messaging tools for communication
+    // Sends message to content script to make the workbench visible
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { action: 'toggle-workbench-visibility' }, (response) => {
+        if (response && response.status === 'success') {
+          
+        }
+      });
+    });
+  }
+
+  function handleSettings() {
+    window.open("chrome-extension://chopojdojpdjgeomjiiinnaabcmieijc/options.html", "_blank")
+  }
+
+  function handleHeadToWebsite() {
+    window.open("https://www.wikipedia.org/", "_blank")
+  }
+
+
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -29,14 +51,14 @@ function IndexPopup() {
           <Typography variant="h6" margin="0px">
             CoQuest
           </Typography>
-          <IconButton sx={{ marginLeft: "auto" }}>
+          <IconButton sx={{ marginLeft: "auto" }} onClick={handleSettings}>
             <SettingsIcon/>
           </IconButton>
         </Toolbar>
       </AppBar>
       <Stack position="absolute" sx={{ bottom: "0px", margin: "0px" }} spacing={5} width="95%" height="70%" direction="column" alignItems="stretch" justifyContent="flex-start">
-        <Button variant="contained" size="large">Explore Topics</Button>
-        <Button variant="contained" size="large">Head to Website</Button>
+        <Button variant="contained" size="large" onClick={handleExploreTopics}>Explore Topics</Button>
+        <Button variant="contained" size="large" onClick={handleHeadToWebsite}>Head to Website</Button>
       </Stack>
     </Box>
   )
